5e00d641a3364445efdaef255557852ac19effb8
Alexander Biryukov <74318440+AlexFromNorth@users.noreply.github.com>
2023-08-30 12:56:29 +0600

    added form valid

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,19 @@
 import "../scss/style.scss";
 import "../scss/media.scss";
+import "../scss/hamburger-menu.scss";
 
+// swiper
 import Swiper from "swiper/bundle";
 
-// import styles bundle
 import "swiper/css/bundle";
 
-// init Swiper:
 const swiper = new Swiper(".swiper", {
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -27,9 +25,39 @@ const swiper = new Swiper(".swiper", {
   spaceBetween: 50,
   breakpoints: {
     765: {
-      //  spaceBetween: 50,
        slidesPerView: 2,
        spaceBetween: 100,
     }
   },
+});
+
+
+// form
+
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("myForm");
+
+  form.addEventListener("submit", function(event) {
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      const invalidInputs = form.querySelectorAll(":invalid");
+      invalidInputs.forEach(input => {
+        input.classList.add("invalid");
+      });
+    } else {
+      event.preventDefault();
+      console.log("Form submitted successfully!");
+      console.log("Username:", form.name.value);
+      console.log("Email:", form.email.value);
+      console.log("Message:", form.text.value);
+    }
+  });
+
+  form.addEventListener("input", function(event) {
+    if (event.target.validity.valid) {
+      event.target.classList.remove("invalid");
+    }
+  });
 });
